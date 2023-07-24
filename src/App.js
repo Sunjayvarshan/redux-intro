517304d3030115d7039ae8f6ea9758416c8ab0bf
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-07-24 15:10:03 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import CreateCustomer from "./CreateCustomer";
+import Customer from "./Customer";
+import AccountOperations from "./AccountOperations";
+import BalanceDisplay from "./BalanceDisplay";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+      <Customer />
+      <AccountOperations />
+      <BalanceDisplay />
     </div>
   );
 }
